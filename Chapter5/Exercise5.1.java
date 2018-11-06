6d94f9e744545e2306d2e80031d605f6ebd25fdc
milouCodes <44801144+milouCodes@users.noreply.github.com>
2018-11-06 15:14:16 +0100

    Create Exercise5.1.java

---
+++ Chapter5/Exercise5.1.java
@@ -0,0 +1,10 @@
+//
+//  Exercise 5.1
+//  ThinkJava - Chapter 5
+//
+//  Created by Milou Zwart on 6/11/2018.
+//
+
+if (x > 0 && x < 10) {
+        System.out.println("positive single digit number.");
+    }
