6956471317ac406b9fb01b140ab401db55411748
milouCodes <44801144+milouCodes@users.noreply.github.com>
2018-11-06 13:45:12 +0100

    Create Exercise4.2.java

---
+++ Chapter4/Exercise4.2.java
@@ -0,0 +1,12 @@
+//
+//  Exercise 4.2
+//  ThinkJava - Chapter 4
+//
+//  Created by Milou Zwart on 6/11/2018.
+//
+
+public static void zool(int one, String two, String three){
+    // your code goes here
+}
+
+zool(11, "pet", "street");
