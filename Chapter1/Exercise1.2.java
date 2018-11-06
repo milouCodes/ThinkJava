2082fd09fdd2938bf8321a693c7f2c4d8873ce7f
milouCodes <44801144+milouCodes@users.noreply.github.com>
2018-11-06 10:36:41 +0100

    Create Exercise1.2.java

---
+++ Chapter1/Exercise1.2.java
@@ -0,0 +1,15 @@
+//
+//  Exercise 1.2
+//  ThinkJava - Chapter 1
+//
+//  Created by Milou Zwart on 6/11/2018.
+//
+
+public class Chapter1 {
+
+    public static void main(String[] args) {
+        // the hello word program
+        System.out.println("Hello, World!");
+        System.out.println("How are you?");
+    }
+}
