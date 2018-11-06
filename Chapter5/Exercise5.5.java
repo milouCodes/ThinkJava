902acd5afb1b8ac51a83c0b66a3747f5bafa6639
milouCodes <44801144+milouCodes@users.noreply.github.com>
2018-11-06 16:39:17 +0100

    Create Exercise5.5.java

---
+++ Chapter5/Exercise5.5.java
@@ -0,0 +1,28 @@
+//
+//  Exercise 5.5
+//  ThinkJava - Chapter 5
+//
+//  Created by Milou Zwart on 6/11/2018.
+//
+public class bottlesofbeer {
+
+    public static void lyrics(int beerNumber) {
+
+        if (beerNumber> 0) {
+            System.out.println(beerNumber + " bottles of beer on the wall,");
+            System.out.println(beerNumber + " bottles of beer,");
+            System.out.println("ya’ take one down, ya’ pass it around,");
+            System.out.print(beerNumber-- + " bottles of beer on the wall.\n\n"); // put whitespace between verses
+            lyrics(beerNumber);
+        }
+
+        else {
+            System.out.print("No bottles of beer on the wall,\nno bottles of beer,\nya’ can’t take one down, ya’ can’t pass it around,\n’cause there are no more bottles of beer on the wall!"); // can also use separate println for every new line
+        }
+    }
+
+    public static void main(String args[]) {
+        lyrics(3);
+    }
+
+}
