66211d3855085bf339fba5b10459918e78de69d4
milouCodes <44801144+milouCodes@users.noreply.github.com>
2018-11-09 12:27:59 +0100

    Create Exercise6.4.java

---
+++ Chapter6/Exercise6.4.java
@@ -0,0 +1,22 @@
+//
+//  Exercise 6.4
+//  ThinkJava - Chapter 6
+//
+//  Created by Milou Zwart on 9/11/2018.
+//
+public class multi {
+
+    public static void main(String arg[]) {
+
+        System.out.println(Multadd(1.0, 2.0, 3.0));
+        // didn't do 4 and 5, because same idea, more maths
+
+    }
+
+    public static double Multadd(double a, double b, double c){
+
+        return a * b + c;
+
+    }
+
+}
